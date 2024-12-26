ef440b00638067c752aa6c98385a4961b8922137
vasanth <vasanth2003s@gmail.com>
2024-12-26 11:34:15 +0530

    Add node_modules

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    parser: "@babel/eslint-parser", // Ensure you're using the right parser
+    parserOptions: {
+      ecmaVersion: 12, // ECMAScript version
+      sourceType: "module",
+    },
+    rules: {
+      // Define any custom rules you may need
+    },
+  };
+  
